6de68f9fa672654d0f281c8258cdba91c72cb70d
Andrei Lovisteanu <lovisteanuandrei@gmail.com>
2023-08-06 22:27:41 +0300

    starting react learn

---
+++ JSremainder/scripts/app.js
@@ -0,0 +1,18 @@
+import * as util from "./util";
+
+
+console.log(util.apyKey);
+
+function ceva(t)  {
+    let x = t.map((item) => ({val:item}))
+    return x;
+}
+
+function transformToObjects(numberArray) {
+    let x = numberArray.map((item) => ({val:item}))
+    return x;
+}
+
+
+util.numberString(util.apyKey);
+
